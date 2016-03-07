8136e05b855a40aea68e77b85cb9fb9feeb7a468
Cmaster14 <cmceuen15@gmail.com>
2016-03-07 15:21:27 -0600

    iOS Update
    Search results changed, but not fully functional and part picker page fully functioning with no extraneous text.

---
+++ PartNamePickerViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Foundation;
+using UIKit;
+
+namespace App.iOS
+{
+	public class PartNamePickerViewModel : UIPickerViewModel
+	{
+		List<string> items;
+		UIButton selectedButton;
+		UIButton searchButton;
+
+		public PartNamePickerViewModel (List<string> pickerData, UIButton pickerButton, UIButton searchForItemsButton)
+		{
+			items = pickerData;
+
+			selectedButton = pickerButton;
+			searchButton = searchForItemsButton;
+		}
+
+		public override nint GetComponentCount (UIPickerView pickerView)
+		{
+			return 1;
+		}
+
+		public override nint GetRowsInComponent (UIPickerView pickerView, nint component)
+		{
+			return items.Count;
+		}
+
+		public override string GetTitle (UIPickerView pickerView, nint row, nint component)
+		{
+			var item = items [(int) row];
+			return item;
+		}
+
+		public override void Selected (UIPickerView pickerView, nint row, nint component)
+		{
+			SearchParameters.PartName = items [(int) row];
+
+			selectedButton.Hidden = false;
+			searchButton.Hidden = false;
+			pickerView.Hidden = true;
+		}
+
+		public override NSAttributedString GetAttributedTitle (UIPickerView pickerView, nint row, nint component)
+		{
+			var title = items [(int) row];
+			int hyphenIndex = title.IndexOf ("-"); // Added to attempt to solve issue with displaying -xx in picker
+			if (hyphenIndex > 0)
+				title = title.Substring (0, hyphenIndex);
+			var font = UIFont.FromName ("SegoeUI-Light", 17f);
+			var attributedTitle = new NSAttributedString (title, font, UIColor.White, null, null, null, (NSLigatureType) 1, 0, (NSUnderlineStyle) 0, null, 0, NSUnderlineStyle.None);
+
+			return attributedTitle;
+		}
+	}
+}
